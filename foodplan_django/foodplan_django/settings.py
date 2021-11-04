86b5fa84eb4d04967874a659301db3f57f0637af
alex <almost@gmx.de>
2021-11-04 11:08:09 +0100

    creating recipes app with models

--- foodplan_django/foodplan_django/settings.py
+++ foodplan_django/foodplan_django/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app.apps.AppConfig',
+    'recipes.apps.RecipesConfig',
     'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ foodplan_django/recipes/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    background_color = models.IntegerField()
+    image = models.ImageField()
+    ingredients = models.ManyToManyField(Ingredient)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self) -> str:
+        return self.name
